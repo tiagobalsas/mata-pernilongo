a5a636901d64c36d064cbcc14141e3976bb07d7e
tiagobalsas <tiago881@gmail.com>
2020-07-10 12:53:58 -0300

    Criando recursos para inselir valor nos elementos html, e capturando estilo css.

--- js/game.js
+++ js/game.js
@@ -8,5 +8,26 @@ function ajustaTela() {
     largura =  window.innerWidth
     console.log(altura,largura)
 }
-
 ajustaTela()
+
+// Criando posições randomincas
+
+var posicaoY = Math.floor(Math.random() * altura) - 90
+var posicaoX = Math.floor(Math.random() * largura) - 90
+
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+
+console.log(posicaoY,posicaoX)
+
+// Criando elemneto html atrvés do DOM.
+
+var pernilongo = document.createElement('img')
+pernilongo.src = 'img/pernilongo.png'
+pernilongo.className = 'pernilongo-tamanho'
+pernilongo.style.left = posicaoX + 'px'
+pernilongo.style.top = posicaoY + 'px'
+pernilongo.style.position = 'absolute'
+
+document.body.appendChild(pernilongo)
+
