bea2675df10200c22b52899a07a2b99d833cab89
tiagobalsas <tiago881@gmail.com>
2020-07-10 17:50:36 -0300

    Aplicando o recurso de criar e remover o pernilongo.

--- js/game.js
+++ js/game.js
@@ -1,35 +1,48 @@
 /* Criando a lógica para redimensionar a tela */
 
-var altura = 0
-var largura =0
+function posicaoRandomica() {
 
-function ajustaTela() {
-    altura = window.innerHeight
-    largura =  window.innerWidth
-    console.log(altura,largura)
+    //remover o pernilongo anterior (caso ele existir)
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
+
+
+
+    var altura = 0
+    var largura =0
+    
+    function ajustaTela() {
+        altura = window.innerHeight
+        largura =  window.innerWidth
+        console.log(altura,largura)
+    }
+    ajustaTela()
+    
+    // Criando posições randomincas
+    
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    
+    console.log(posicaoY,posicaoX)
+    
+    // Criando elemneto html atrvés do DOM.
+    
+    var pernilongo = document.createElement('img')
+    pernilongo.src = 'img/pernilongo.png'
+    pernilongo.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    pernilongo.style.left = posicaoX + 'px'
+    pernilongo.style.top = posicaoY + 'px'
+    pernilongo.style.position = 'absolute'
+    pernilongo.id = 'mosquito'
+    
+    document.body.appendChild(pernilongo)
+
 }
-ajustaTela()
 
-// Criando posições randomincas
-
-var posicaoY = Math.floor(Math.random() * altura) - 90
-var posicaoX = Math.floor(Math.random() * largura) - 90
-
-posicaoY = posicaoY < 0 ? 0 : posicaoY
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-
-console.log(posicaoY,posicaoX)
-
-// Criando elemneto html atrvés do DOM.
-
-var pernilongo = document.createElement('img')
-pernilongo.src = 'img/pernilongo.png'
-pernilongo.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
-pernilongo.style.left = posicaoX + 'px'
-pernilongo.style.top = posicaoY + 'px'
-pernilongo.style.position = 'absolute'
-
-document.body.appendChild(pernilongo)
 
 
 // Função para criar tamanhos randomincos do penilongo
